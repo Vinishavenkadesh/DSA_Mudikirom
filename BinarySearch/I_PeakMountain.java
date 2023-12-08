6c32b4d3e9887aa41c7ded3e49d6cad74961a588
Venkadesh <vinisha.venkadesh@worldline.com>
2023-12-08 23:03:35 +0530

    Binary Search

--- BinarySearch/I_PeakMountain.java
+++ BinarySearch/I_PeakMountain.java
@@ -3,10 +3,10 @@ package BinarySearch;
 public class I_PeakMountain {
     public static void main(String[] args) {
         int [] arr = {0,3,5,12,2};
-        System.out.println(searchingIndex(arr));
+        System.out.println(peak(arr));
     }
 
-    public static int searchingIndex(int [] arr){
+    public static int peak(int [] arr){
         int start = 0;
         int end = arr.length-1;
         while(start < end){
--- BinarySearch/K_RotatedSorted.java
+++ BinarySearch/K_RotatedSorted.java
@@ -2,44 +2,53 @@ package BinarySearch;
 
 public class K_RotatedSorted {
     public static void main(String[] args) {
-        int [] nums = {5,1,3};
-        int target = 5;
-        // System.out.println(findingAnswer(nums, target));
-        System.out.println(peak(nums));
+        int [] arr = {4,5,6,7,0,1,2};
+        int target = 0;
+        System.out.println(search(arr, target));
+        
     }
 
-    static int findingAnswer(int [] arr,int target){
-        int peak = peak(arr);
-        int firstTry = binarySearch(arr, target, 0, peak);
-        if(firstTry != -1)
+    public static int search(int [] arr, int target){
+        int pivot = pivot(arr);
+        if(target == pivot)
+        return pivot;
+        else if(pivot == -1)
+        return binarySearch(arr, target, 0, arr.length - 1);
+        int firstTry = binarySearch(arr, target, 0, pivot-1);
+        if(firstTry!= -1)
         return firstTry;
         else
-        return binarySearch(arr, target, peak+1, arr.length-1);
+        binarySearch(arr, target, pivot+1, arr.length - 1);
+        return -1;
+    }
+    
+    public static int pivot(int [] arr){
+        int start = 0;
+        int end = arr.length - 1;
+        while(start <= end){
+            int mid = start + (end - start)/2;
+            if(end > mid + 1 && arr[mid] > arr[mid+1])
+            return mid;
+            else if(start < mid - 1 && arr[mid] < arr[mid - 1])
+            return mid - 1;
+            else if(arr[start] > arr[mid])
+            end = mid - 1;
+            else
+            start = mid + 1; 
+        }
+        return -1;
     }
 
-    static int binarySearch(int [] arr,int target,int start,int end){
+    public static int binarySearch(int [] arr,int target,int start,int end){
         while(start <= end){
             int mid = start + (end - start)/2;
             if(target > arr[mid])
             start = mid + 1;
             else if(target < arr[mid])
             end = mid - 1;
-            else
+            else 
             return mid;
         }
         return -1;
-    }
-    
-    static int peak(int [] arr){
-        int start = 0;
-        int end = arr.length - 1;
-        while(start < end){
-            int mid = start+(end - start)/2;
-            if(arr[mid] > arr[mid+1])
-            end = mid;
-            else
-            start = mid+1;
-        }
-        return arr[start];
     }
 }
